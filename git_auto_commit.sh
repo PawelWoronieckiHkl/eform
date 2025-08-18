6e693ba92e9da8dc438a8e44d59e259b05bf7835
PawelWrHkl <pawel.woroniecki.hkl@gmail.com>
2025-08-18 12:59:44 +0200

    Add node_modules to .gitignore and remove from repo

--- git_auto_commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/pawel/projects/eform
-
-git add .
-git commit -m "Automatic commit $(date '+%Y-%m-%d %H:%M')"
-git push origin main
--- public/scripts/checkJsVer.js
+++
@@ -1,9 +0,0 @@
-function jsCheck() {
-    try {
-        // Próba użycia nowej składni
-        eval("var test = {}?.a");
-        console.log('Przeglądarka aktualna')
-    } catch (e) {
-        alert("Twoja przeglądarka jest przestarzała i wymaga aktualizacji!");
-    }
-}
